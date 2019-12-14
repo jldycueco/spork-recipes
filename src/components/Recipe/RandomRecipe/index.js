79beba5536abed781ca7ff6090b091a386fafc9c
JD <laeim017@gmail.com>
2019-12-14 19:27:20 +0800

    Added ingredients and instructions

--- src/components/Recipe/RandomRecipe/index.js
+++ src/components/Recipe/RandomRecipe/index.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { SearchContext } from '../../../context/SearchContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const RandomRecipe = () => {
   const { fetchedData, isError, isLoading, setUrl } = useContext(
@@ -10,7 +11,32 @@ const RandomRecipe = () => {
     setUrl(`https://www.themealdb.com/api/json/v1/1/random.php`);
   }, [setUrl]);
 
+  let ingredientMeasurement = [];
+
+  if (Object.keys(fetchedData).length > 0) {
+    let p = fetchedData.meals[0];
+
+    let ingredientArray = [];
+    let measurementArray = [];
+
+    for (let key in p) {
+      if (p.hasOwnProperty(key)) {
+        if (key.includes('strIngredient') && p[key].length > 0) {
+          ingredientArray.push(p[key]);
+        } else if (key.includes('strMeasure') && p[key].length > 0) {
+          measurementArray.push(p[key]);
+        }
+      }
+    }
+
+    ingredientMeasurement = ingredientArray.map(
+      (ingredient, index) =>
+        measurementArray[index] + ' ' + ingredientArray[index],
+    );
+  }
+
   console.log(fetchedData);
+
   return (
     <>
       {isError && <div>Error</div>}
@@ -21,7 +47,26 @@ const RandomRecipe = () => {
           <>
             <div>{fetchedData.meals[0].strMeal}</div>
             <img src={fetchedData.meals[0].strMealThumb} alt="img" />
-            <div>{fetchedData.meals[0].strInstructions}</div>
+            <a href={fetchedData.meals[0].strYoutube}>
+              <FontAwesomeIcon icon={['fab', 'youtube']} />
+            </a>
+            <a href={fetchedData.meals[0].strSource}>
+              <FontAwesomeIcon icon="external-link-alt" />
+            </a>
+
+            <ul>
+              {ingredientMeasurement.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ul>
+
+            <ul>
+              {fetchedData.meals[0].strInstructions
+                .split('. ')
+                .map((strInstruction, index) => (
+                  <li key={index}>{strInstruction}</li>
+                ))}
+            </ul>
           </>
         )
       )}
--- src/components/Recipe/SpecificRecipe/index.js
+++ src/components/Recipe/SpecificRecipe/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useAxios from '../../../customhooks/useAxios';
 import { useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const SpecificRecipe = () => {
   const { id } = useParams();
@@ -9,6 +10,30 @@ const SpecificRecipe = () => {
     {},
     `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
   );
+
+  let ingredientMeasurement = [];
+
+  if (Object.keys(fetchedData).length > 0) {
+    let p = fetchedData.meals[0];
+
+    let ingredientArray = [];
+    let measurementArray = [];
+
+    for (let key in p) {
+      if (p.hasOwnProperty(key)) {
+        if (key.includes('strIngredient') && p[key].length > 0) {
+          ingredientArray.push(p[key]);
+        } else if (key.includes('strMeasure') && p[key].length > 0) {
+          measurementArray.push(p[key]);
+        }
+      }
+    }
+
+    ingredientMeasurement = ingredientArray.map(
+      (ingredient, index) =>
+        measurementArray[index] + ' ' + ingredientArray[index],
+    );
+  }
 
   return (
     <>
@@ -20,7 +45,26 @@ const SpecificRecipe = () => {
         <>
           <div>{fetchedData.meals[0].strMeal}</div>
           <img src={fetchedData.meals[0].strMealThumb} alt="img" />
-          <div>{fetchedData.meals[0].strInstructions}</div>
+          <a href={fetchedData.meals[0].strYoutube}>
+            <FontAwesomeIcon icon={['fab', 'youtube']} />
+          </a>
+          <a href={fetchedData.meals[0].strSource}>
+            <FontAwesomeIcon icon="external-link-alt" />
+          </a>
+
+          <ul>
+            {ingredientMeasurement.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+
+          <ul>
+            {fetchedData.meals[0].strInstructions
+              .split('. ')
+              .map((strInstruction, index) => (
+                <li key={index}>{strInstruction}</li>
+              ))}
+          </ul>
         </>
       ) : (
         <div>Error</div>
